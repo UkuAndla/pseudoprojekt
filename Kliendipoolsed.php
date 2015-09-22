82d682e3b88923aed5c4fb1e405985218e4a3363
ukuandla <ukuandla@gmail.com>
2015-09-22 16:28:16 +0300

    6. jQuery kass koeraks [Delivers #103901126]

--- Kliendipoolsed.php
+++ Kliendipoolsed.php
@@ -2,7 +2,9 @@
 <html lang="et">
 <head>
     <meta charset="UTF-8">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
     <title>Kliendipoolsed</title>
 </head>
 <body>

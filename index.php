19f34da1cd3f7cf3b165af50ffd4fa875b2f97b6
ukuandla <ukuandla@gmail.com>
2015-09-29 11:32:18 +0300

    29. Multidimensionaalne massiiv [Delivers #104409424]

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<?php $posts=array(
+<?php
+$posts[]=array(
         'title'=>'Man must explore, and this is exploration at its greatest',
         'subtitle'=>'Problems look mighty small from 150 miles up',
         'author'=>'Start Bootstrap',
         'date'=>'September 24, 2014'
-        )?>
+);
+$posts[]=array(
+            'title'=>'I believe every human has a finite number of heartbeats.',
+            'subtitle'=>"I don't intend to waste any of mine.",
+            'author'=>'Start Bootstrap',
+            'date'=>'September 18, 2014')
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -94,27 +101,30 @@
 
     <!-- Main Content -->
     <div class="container">
+        <?foreach($posts as $post):?>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="post-preview">
+                < class="post-preview">
                     <a href="post.html">
                         <h2 class="post-title">
-                            <?=$posts['title']?>
+                            <?=$post['title']?>
                         </h2>
                         <h3 class="post-subtitle">
-                            <?=$posts['subtitle']?>>
+                            <?=$post['subtitle']?>
                         </h3>
                     </a>
-                    <p class="post-meta">Posted by <a href="#"><?=$posts['author']?></a> on <?=$posts['date']?></p>
+                    <p class="post-meta">Posted by <a href="#"><?=$post['author']?></a> on <?=$post['date']?></p>
 
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="next">
-                        <a href="#">Older Posts &rarr;</a>
-                    </li>
-                </ul>
             </div>
+            <?endforeach?>
+            <hr>
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="next">
+                    <a href="#">Older Posts &rarr;</a>
+                </li>
+            </ul>
+
         </div>
     </div>
 

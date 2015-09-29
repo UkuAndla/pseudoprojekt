9ff22df487dcd933c25df65914ab5b859e538929
ukuandla <ukuandla@gmail.com>
2015-09-29 11:03:15 +0300

    24. Require refactor [Delivers #104406548]

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    $page=(isset($_GET['page'])? $_GET['page'] : 'home');
+?>
+
 <!doctype html>
 <html lang="et">
 <head>
@@ -6,9 +10,7 @@
     </head>
 <body>
 <?php
-    if(isset($_GET['page'])){
-            require $_GET['page'].".php";
-        }
+  require $page.".php";
 ?>
 +</body>
 +</html>

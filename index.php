3585e72de7697e513eb068e257c7c53a8221e1cc
ukuandla <ukuandla@gmail.com>
2015-09-29 11:00:54 +0300

    23. Require [Delivers #104406540]

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-tere maailm
+<!doctype html>
+<html lang="et">
+<head>
+        <meta charset="UTF-8">
+        <title>Serveripoolsed</title>
+    </head>
+<body>
+<?php
+    if(isset($_GET['page'])){
+            require $_GET['page'].".php";
+        }
+?>
++</body>
++</html>

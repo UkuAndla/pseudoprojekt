241b98184117e7ab5387ae65beeb49b83a53dc1e
ukuandla <ukuandla@gmail.com>
2015-10-22 15:48:47 +0300

    Index.php parandamine [Delivers #106298424]

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+lines (161 sloc)  6.25 KB
 <?php
 require('config.php');
-$db=mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die('Could not connect!');
+$db=mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db,"SET NAMES 'utf8'");
-$q= mysqli_query($db,"SELECT * FROM posts JOIN authors");
+$q= mysqli_query($db,"SELECT * FROM posts JOIN authors ON posts.author_id=authors.authors_id");
 while($row=mysqli_fetch_assoc($q)){
     $posts[]=$row;
 }

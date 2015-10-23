eb8cbd314d934a11332f62dbfc62a25d6bb7193b
ukuandla <ukuandla@gmail.com>
2015-10-23 15:07:10 +0300

    37. Emaili valideerimine serveripoolel ka [delivers #106475932]

--- api.php
+++ api.php
@@ -1,13 +1,12 @@
 <?php
-    if(isset($_GET['emailvalidation'])){
-        $meil=$_GET['emailvalidation'];
-        $string2Array=explode("@",$meil);
-        if(checkdnsrr(array_pop($string2Array),"MX")){
-            exit("Emaili domeen eksisteerib.");
-        }
-        else{
-            exit("Emaili domeeni ei ole olemas.");
-        }
+if(isset($_GET['emailvalidation'])){
+            $meil=$_GET['emailvalidation'];
+            if(checkdnsrr(array_pop(explode("@",$meil)),"MX")){
+                    exit("Emaili domeen eksisteerib.");
+                }
+            else{
+                    exit("Emaili domeeni ei ole olemas.");
+                }
     }
     $filename="data.csv";
     if(file_exists($filename)){
--- meiliaadressi_kontrollimine.php
+++ meiliaadressi_kontrollimine.php
@@ -1,6 +1,9 @@
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.js"></script>
 <input type="text" id="meil" placeholder="sinumeil@veebiaadress.lühend" size="25" onkeypress="Javascript: if (event.keyCode==13) kontrolliEmaili();"/>
 <input type="submit" value="Kontrolli" name="kontrolli" onclick="Javascript:kontrolliEmaili();"/>
+<div id="result"></div>
 <script type="text/javascript">
+
         function kontrolliEmaili(){
                 var meil = document.getElementById('meil');
                 var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -10,8 +13,12 @@
                         return false;
                     }
                 else{
-                        alert('Sisestatud meil on korrektne!');
-                        return true;
+
+                    $.get('api.php',{emailvalidation:meil.value},function(data){
+                        +                $('#result').text(data);
+                        +            });
+                    return true;
+
                     }
             }
     </script>

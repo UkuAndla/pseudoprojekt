4bc89f3b1ccc83df774941acb1569025b70af58d
ukuandla <ukuandla@gmail.com>
2015-10-26 10:49:56 +0200

    38. Tabeli sorteerimine [delivers #106625014]

--- klient.php
+++ klient.php
@@ -4,12 +4,15 @@
         border-collapse: collapse;
         padding:5px;
     }
+        th{
+                width:150px;
+            }
 </style>
-<table>
+<table id="table">
     <tr>
-        <th>Eesnimi</th>
-        <th>Perenimi</th>
-        <th>Email</th>
+        <th onclick="Javascript:sortByColumn(this.cellIndex);">Eesnimi</th>
+        <th onclick="Javascript:sortByColumn(this.cellIndex);">Perenimi</th>
+        <th onclick="Javascript:sortByColumn(this.cellIndex);">Email</th>
     </tr>
 </table>
 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.js">
@@ -22,4 +25,31 @@
             $('table').append("<tr><td>"+j[0]+"</td><td>"+j[1]+"</td><td>"+j[2]+"</td></tr>");
         }
     })
+        function sortByColumn(data){
+                    var data=data;
+                    var table = document.getElementById('tabel');
+                    var tableData = table.getElementsByTagName('tbody').item(0);
+                    var rowData = tableData.getElementsByTagName('tr');
+                    var getTheader=document.getElementById('tabel').getElementsByTagName('tbody').item(0).getElementsByTagName('tr').item(0).getElementsByTagName('th').item(data)
+                        getTheader.classList.toggle('asc');
+                    if(getTheader.classList[0]=='asc'){
+                            for(var i = 0; i < rowData.length - 1; i++){
+                                    for(var j = 1; j < rowData.length - (i + 1); j++){
+                                            if(rowData.item(j).getElementsByTagName('td').item(data).innerHTML > rowData.item(j+1).getElementsByTagName('td').item(data).innerHTML){
+                                                    tableData.insertBefore(rowData.item(j+1),rowData.item(j));
+                                                }
+                                        }
+                                }
+                        }
+                    else{
+                            for(var i = 0; i < rowData.length - 1; i++){
+                                    for(var j = 1; j < rowData.length - (i + 1); j++){
+                                            if(rowData.item(j).getElementsByTagName('td').item(data).innerHTML < rowData.item(j+1).getElementsByTagName('td').item(data).innerHTML){
+                                                    tableData.insertBefore(rowData.item(j+1),rowData.item(j));
+                                                }
+                                        }
+                                }
+                        }
+                };
+
     </script>
